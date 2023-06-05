aaf7c9d8f9ea11b818bf6d798d8502067576451a
Pradeep Gupta <49131135+Zshader@users.noreply.github.com>
2023-06-06 01:43:23 +0530

    age chekc

---
+++ Age-check.py
@@ -0,0 +1,8 @@
+age = input("Hey, \n \t Welcome to ABC. \n Please enter you age: ")
+
+if int(age) > 18:
+    print("You are eligible")
+elif int(age) == 18:
+    print("You are eligible") 
+else:
+    print("Sorry, you are not eligible")
